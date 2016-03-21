05c6e496e98a4b1abe590d8e4df8eb318dd0a383
roman.zheleznov@maxymiser.com <9Gsu2$rY@e5^Gh@G>
2016-03-21 22:03:56 +0200

    finish about page, start with contacts

--- app/src/components/about.js
+++ app/src/components/about.js
@@ -1,7 +1,15 @@
 export default class About extends React.Component {
     render(){
         return (
-            <h1>About Page</h1>
+            <div className="mdl-grid">
+                <div className="mdl-cell mdl-cell--1-col"></div>
+                <div className="mdl-cell mdl-cell--8-col">
+                    <h5>About</h5>
+                    <p>Welcome to the Flight Schedule. This application can help you to get information about planes schedule for any destinations</p>
+                    <p>We try to create use full ang lightweight app for our clients. If you have qiestion please contact Us</p>
+                    <p>Sincerely yours, Flight Schedule App Team</p>
+                </div>
+            </div>
         )
     }
 }
--- app/src/components/contacts.js
+++ app/src/components/contacts.js
@@ -1,7 +1,31 @@
 export default class Contacts extends React.Component {
-    render(){
+    render() {
         return (
-            <h1>Contacts Page</h1>
+            <div className="mdl-grid contacts-page">
+                <div className="mdl-cell mdl-cell--1-col"></div>
+                <div className="mdl-cell mdl-cell--8-col">
+                    <h5>Contacts</h5>
+                    <p>Feel free to send your opinion about the app. Or maybe you notice some bugs or have an idea how
+                        to make this app better, let Us know :)</p>
+                    <form className="contacts-form" action="">
+                        <div className="mdl-textfield mdl-js-textfield">
+                            <input className="mdl-textfield__input name" name="name" type="text" id="name"/>
+                            <label className="mdl-textfield__label" for="name">Your Name...</label>
+                        </div>
+                        <div className="mdl-textfield mdl-js-textfield">
+                            <input className="mdl-textfield__input email" name="email" type="email" id="email"/>
+                            <label className="mdl-textfield__label" for="email">example@site.com</label>
+                        </div>
+                        <div className="mdl-textfield mdl-js-textfield">
+                            <textarea className="mdl-textfield__input" type="text" rows="3" id="message"></textarea>
+                            <label className="mdl-textfield__label" for="message">Your message goes here...</label>
+                        </div>
+                        <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored send-message">
+                            Send message
+                        </button>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
